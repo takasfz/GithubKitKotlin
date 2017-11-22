cfc2be578dd8f3934f50ce6877bc3c08c701c398
Takashi Kinjo <12707307+takasfz@users.noreply.github.com>
2017-11-22 20:08:34 +0900

    test

--- GithubKit/src/androidTest/java/com/takasfz/githubkit/ExampleInstrumentedTest.java
+++ GithubKit/src/androidTest/java/com/takasfz/githubkit/ExampleInstrumentedTest.java
@@ -3,11 +3,16 @@ package com.takasfz.githubkit;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
+import com.takasfz.githubkit.core.response.Response;
+import com.takasfz.githubkit.core.response.User;
+
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,6 +25,10 @@ public class ExampleInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
+
+        JSONObject json = new JSONObject("{\"data\":{\"search\":{\"nodes\":[{\"avatarUrl\":\"https://avatars3.githubusercontent.com/u/478864?v=4\",\"bio\":\"\",\"followers\":{\"totalCount\":68},\"following\":{\"totalCount\":1},\"id\":\"MDQ6VXNlcjQ3ODg2NA==\",\"location\":\"Portland, OR\",\"login\":\"martypdx\",\"repositories\":{\"totalCount\":163},\"url\":\"https://github.com/martypdx\",\"websiteUrl\":\"\"}],\"pageInfo\":{\"endCursor\":\"Y3Vyc29yOjU=\",\"hasNextPage\":true,\"hasPreviousPage\":false,\"startCursor\":\"Y3Vyc29yOjE=\"},\"userCount\":2778}}}");
+        Response response = new Response(User.getDecoder(), new String[]{ "search" }, "userCount", json);
+        Log.d("", response.toString());
 
         assertEquals("com.takasfz.githubkit.test", appContext.getPackageName());
     }
